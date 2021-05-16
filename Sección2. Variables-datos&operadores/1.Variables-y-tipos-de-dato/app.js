0bb953a79393d2a95e719bb0acf560b40c6245ff
AlexeerdXD <ever.condori@upeu.edu.pe>
2021-05-15 21:10:48 -0500

    Seccion 2, variables

---
+++ Sección2. Variables-datos&operadores/1.Variables-y-tipos-de-dato/app.js
@@ -0,0 +1,24 @@
+// Uso  de Variables 
+let nombre = 'Grover';
+console.log(nombre);
+console.log(typeof(nombre));
+
+let edad = 36;
+console.log(edad);
+console.log(typeof(edad));
+
+edad = 'Cinco';
+console.log(edad);
+console.log(typeof(edad));
+
+let sueldo  = 1990.99;
+console.log(sueldo);
+console.log(typeof(sueldo));
+
+let tieneTrabajo = false;
+console.log(tieneTrabajo);
+
+let puestodeTrabajo;
+console.log(puestodeTrabajo);
+puestodeTrabajo = null;
+console.log(puestodeTrabajo);
