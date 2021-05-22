f0e5037b246d369efe96d29126235fc99cb6e67a
AlexeerdXD <ever.condori@upeu.edu.pe>
2021-05-21 19:35:39 -0500

    sentencia if-else

---
+++ Sección3/1. Sentencia if-else/app.js
@@ -0,0 +1,12 @@
+// 
+// Sentencia if - else 
+// 
+
+let nombre = 'Grover';
+let estado = false;
+
+if (estado == true) {
+    console.log(nombre + ' es casado!');
+}else{
+    console.log(nombre + ' no es casado!');
+}
