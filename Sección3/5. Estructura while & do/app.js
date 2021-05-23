e9447356c3c614f9db8e5b30b262d6570b96a0d2
AlexeerdXD <ever.condori@upeu.edu.pe>
2021-05-22 19:50:52 -0500

    Estructura while & do...while

--- Sección3/5. Estructura while & do/app.js
+++ Sección3/5. Estructura while & do/app.js
@@ -1,2 +1,21 @@
 //  ------ Sentencia While, Do while -------
 
+let i = 0; //si esto es = a 25
+
+while (i <= 25) {
+    console.log(i);
+    i+=5;
+} //esto es = a "nada" porque la condicion esta al inicio
+console.log(i);
+
+console.log('----------');
+
+// -------- Sentencia do...while -------
+
+let num = 0; // si esto es = a 100
+do {
+    console.log(num);
+     num+=10;
+} while (num <= 100);//esto es = a 100 porque la condicion esta al final
+
+console.log(num);
